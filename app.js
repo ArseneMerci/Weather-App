d3d9ee5428ec627716941fd36cd486614ebd320a
ArseneMerci <harsenemerci@gmail.com>
2021-05-17 20:13:03 +0200

    displayed first weather condition

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const request = require('request')
+const chalk = require('chalk')
 
 const url = 'http://api.weatherstack.com/current?access_key=5a9a04d7ff075a6df8376fbe66a70c34&query=1.9403,29.8739'
 
-request({url: url}, (err,response)=>{
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({url: url, json: true}, (err,response)=>{
+    const path = response.body.current
+    console.log(chalk.green.inverse(`${path.weather_descriptions}! `) + `It is currently ${path.temperature} degrees out, but it feels like ${path.feelslike} degrees in.`)
 })
