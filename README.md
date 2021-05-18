cb7c0b22db81911af3db0c158b853a0c089e98de
ArseneMerci <harsenemerci@gmail.com>
2021-05-18 11:59:20 +0200

    Displaying weather condition of location inputed through command line

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Weather App
 ## This is an app that displays weather condition in different places
+
+### Using Command Terminal
+
+```
+$ node app.js muhanga rwanda
+Muhanga, Southern, Rwanda
+Partly cloudy! It is currently 23 degrees out, but it feels like 25 degrees in.
+```
+```
+$ node app.js new york
+New York, New York, United States
+Sunny! It is currently 16 degrees out, but it feels like 16 degrees in.
+```
--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const chalk = require('chalk')
 const geoCode = require('./geoCode')
 const foreCast = require('./foreCast')
+const location = require('./location')
 
-
-foreCast('-2.0763603681872','29.7630163282156',(err,data)=>{
-    if(err){
-        console.log(chalk.red.inverse(response.body.error.info))
-    }else
-    console.log(chalk.green.inverse(`${data.descr}! `) + `It is currently ${data.temp} degrees out, but it feels like ${data.feelsLike} degrees in.`)
-})
-// geoCode('muhanga rwanda',(err,data)=>{
-//     if(err){
-//         console.log(chalk.red.inverse(err))
-//     }else
-//     console.log(data.placeName + ', ' + data.latitude + ', ' + data.longitude)
-// })
+if(!location){
+    console.log(chalk.yellow.inverse('Please provide your location!'))
+}else{
+geoCode(location,(err,data)=>{
+    if(err)return console.log(chalk.red.inverse(err))
+    foreCast(data.latitude,data.longitude,(err,forecastData)=>{
+        if(err) return console.log(chalk.red.inverse(response.body.error.info))
+        console.log(data.placeName)
+        console.log(chalk.green.inverse(`${forecastData.descr}! `) + `It is currently ${forecastData.temp} degrees out, but it feels like ${forecastData.feelsLike} degrees in.`)
+    })
+})}
